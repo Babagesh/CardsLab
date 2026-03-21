4dbe87572a744276628a271ac736ba6a064192a7
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2026-03-21 19:38:34 -0400

    added card functionality and logout

---
+++ frontend/src/components/LoggedInName.tsx
@@ -0,0 +1,14 @@
+const LoggedInName = () => {
+    const doLogout = (e:any) => {
+        e.preventDefault();
+        alert('doLogout');
+    };
+    return (
+        <div id="loggedInDiv">
+        <span id="userName">Logged In As John Doe </span><br />
+        <button type="button" id="logoutButton" className="buttons"
+        onClick={doLogout}> Log Out </button>
+        </div>
+    );
+}
+export default LoggedInName;
