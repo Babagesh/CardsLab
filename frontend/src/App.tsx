b0017427c4e793eeb1e7d650bf0bc09285e8aeef
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2026-03-21 23:38:47 -0400

    added frontend routing and state for specific pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import LoginPage from './pages/LoginPage'
 import CardPage from './pages/CardPage'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-      <CardPage />
-
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage/>} />
+          <Route path="/cards" element={<CardPage />}/>
+        </Routes>
+      
+      </BrowserRouter>
+  );
 }
 
 export default App
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,16 +1,29 @@
+import {useState} from 'react'
+
+const [message, setMessage] = useState('')
+const [loginName, setLoginName] = useState('');
+const [loginPassword, setPassword] = useState('');
 export default function Login()
 {
     const doLogin = (e:any) => {
         e.preventDefault();
-        alert('doIt()');
+        alert('doIt()' + loginName + loginPassword);
+    }
+
+    const handleSetUsername = (e:any) => {
+        setLoginName(e.target.value);
+    }
+
+    const handleSetPassword = (e:any) => {
+        setPassword(e.target.value);
     }
     return (
         <div id="loginDiv">
             <span id="inner-title">PlEASE LOG IN</span><br />
-            <input type="text" id="loginName" placeholder="Username" /><br />
-            <input type="password" id="loginPassword" placeholder="Password"/><br />
+            <input type="text" id="loginName" placeholder="Username" onChange={handleSetUsername}/><br />
+            <input type="password" id="loginPassword" placeholder="Password" onChange={handleSetPassword}/><br />
             <input type="submit" id="loginButton" className="buttons" value="Do it" onClick={doLogin}/>
-            <span id="loginResult"></span>
+            <span id="loginResult">{message}</span>
         </div>
     );
 }
